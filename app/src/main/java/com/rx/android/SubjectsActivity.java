ee4435105b50612cfba60e1def2002858f2c7640
charchit28 <kasliwalcharchit@gmail.com>
2017-05-04 00:35:58 +0530

    Final Code commit

--- app/src/main/java/com/rx/android/SubjectsActivity.java
+++ app/src/main/java/com/rx/android/SubjectsActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import rx.Observer;
 import rx.subjects.PublishSubject;
 
